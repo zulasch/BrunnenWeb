c9d3086a3796e1154880c454cb4f5b18f9b54ad4
zulasch <zulasch@github.com>
2025-10-25 00:36:06 +0200

    file updates

--- mosfet_control.py
+++ mosfet_control.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import logging
 import RPi.GPIO as GPIO
+import lgpio
 
+chip = lgpio.gpiochip_open(0)
 CHANNELS = [17, 18, 27, 22, 23, 24, 25, 4]
 _initialized = False
 
@@ -14,11 +17,12 @@ def init_gpio():
             GPIO.output(ch, GPIO.LOW)
         _initialized = True
 
-def set_output(channel_index, state):
-    init_gpio()
-    if 0 <= channel_index < len(CHANNELS):
-        GPIO.output(CHANNELS[channel_index], GPIO.HIGH if state else GPIO.LOW)
-
+def set_output(index, state):
+    handle = lgpio.gpiochip_open(0)
+    lgpio.gpio_claim_output(handle, CHANNELS[index])
+    lgpio.gpio_write(handle, CHANNELS[index], 1 if state else 0)
+    lgpio.gpiochip_close(handle)
+    
 def all_off():
     init_gpio()
     for ch in CHANNELS:
