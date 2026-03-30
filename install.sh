01dde7d5ef3520616f00fcc23703cb875c94e08d
Daniel Frincu <48367102+zulasch@users.noreply.github.com>
2026-03-30 22:21:11 +0200

    MQTT

--- install.sh
+++ install.sh
@@ -117,7 +117,8 @@ fi
 
 source "$VENV_DIR/bin/activate"
 pip install --upgrade pip
-pip install flask psutil influxdb-client adafruit-circuitpython-ads1x15 board RPi.GPIO gunicorn lgpio adafruit-circuitpython-ssd1306 pillow luma.oled adafruit-circuitpython-bmp280 requests
+#pip install flask psutil influxdb-client adafruit-circuitpython-ads1x15 board RPi.GPIO gunicorn lgpio adafruit-circuitpython-ssd1306 pillow luma.oled adafruit-circuitpython-bmp280 requests
+pip install -r requirements.txt
 deactivate
 ok "Python-Abhängigkeiten installiert"
 
